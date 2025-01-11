aef5ae1f8ca1c96b6b8ac6ee55a122e41a1485a2
OlgerQuispe <goldsag43@gmail.com>
2025-01-10 20:15:26 -0500

    update buttonsScreen and update name archive CardsScreen

--- lib/presentation/screens/buttons/buttons_screen.dart
+++ lib/presentation/screens/buttons/buttons_screen.dart
@@ -5,6 +5,11 @@ class ButtonsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Buttons Screen'),
+      ),
+      body: const Placeholder(),
+    );
   }
 }
