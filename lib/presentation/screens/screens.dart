877ff3bf497dc336936c507d446d9ac90ace7c26
OlgerQuispe <goldsag43@gmail.com>
2025-02-02 17:39:20 -0500

    screens created and barril update

--- lib/presentation/screens/screens.dart
+++ lib/presentation/screens/screens.dart
@@ -1,3 +1,10 @@
+export 'package:widgets_apps/presentation/screens/animated/animated_screen.dart';
+export 'package:widgets_apps/presentation/screens/app_tutorial/app_tutorial_screen.dart';
+export 'package:widgets_apps/presentation/screens/infinite_scroll/infinite_scroll_screen.dart';
+export 'package:widgets_apps/presentation/screens/progress/progress_screen.dart';
+export 'package:widgets_apps/presentation/screens/snackbar/snackbar_screen.dart';
+export 'package:widgets_apps/presentation/screens/ui_controls/ui_controls_screen.dart';
+
 export 'package:widgets_apps/presentation/screens/buttons/buttons_screen.dart';
 export 'package:widgets_apps/presentation/screens/cards/cards_screen.dart';
 export 'package:widgets_apps/presentation/screens/home/home_screen.dart';
