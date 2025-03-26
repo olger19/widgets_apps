5644a6769f2e35e0e16f8fd474146361ce3e5aa8
OlgerQuispe <goldsag43@gmail.com>
2025-03-25 22:29:26 -0500

    update snackBarScreen

--- lib/presentation/screens/snackbar/snackbar_screen.dart
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:widgets_apps/config/router/app_router.dart';
 
 class SnackbarScreen extends StatelessWidget {
 
